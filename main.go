4a152a832456b9e9622213defeda97dbc8ad12d2
Maxwell Mbugua <maxwell@maaax.pro>
2023-07-16 13:37:12 +0300

    Uploading a file.

--- main.go
+++ main.go
@@ -1,19 +1,15 @@
 package main
 
-// The following are the packages you need.
-// aws
-// aws/session -> Create an AWS Session using a given region
-// service/s3 -> Working with Amazon S3
-
 import (
     "github.com/aws/aws-sdk-go/aws"
     "github.com/aws/aws-sdk-go/aws/session"
-    "github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
     "fmt"
+	"os"
 )
 
 func main() {
-	// set up the session with the right region
+	// set up the session with the right region and load the credentials.
 	session, err_session := session.NewSession(&aws.Config{
 		Region: aws.String("eu-west-2"),
 	})
@@ -21,16 +17,29 @@ func main() {
 	if err_session != nil {
 		fmt.Printf("%s\n", err_session)
 	}	
-
-	// create an S3 service client
-	svc := s3.New(session)
 	
-	// list buckets
-	bkts, err_bkts := svc.ListBuckets(nil)
+	// the bucket name to which I am uploading the files.
+	bucket_name := "test-bucket-cindy"
 	
-	if err_bkts != nil {
-		fmt.Printf("%s\n", err_bkts)
+	f, err_f := os.Open("cat.jpg")
+	if err_f != nil {
+		fmt.Printf("%s\n", err_f)
+		os.Exit(1)
+	}
+	defer f.Close()
+	
+	// create an new uploader and pass it the session.
+	uploader := s3manager.NewUploader(session)	
+	
+	res, res_err := uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String(bucket_name),
+		Key: aws.String("cat.jpg"),	
+		Body: f,
+	})
+	
+	if res_err != nil {
+		fmt.Printf("%s\n", res_err)
 	}
 	
-	fmt.Printf("%v\n", bkts)
+	fmt.Printf("%v\n", res)
 }
